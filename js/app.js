78eb7412b789cdd6f756209031f3ff8572f9da78
Houda <houdaslassi.18@email.com>
2020-08-03 17:25:02 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,21 +1,31 @@
-const pok_btn = document.getElementById('get_pokemons');
-pok_btn.addEventListener('click', getPokemons);
+const pokedex = document.getElementById("pokedex");
+    	const promises = [];
+		for (let i = 1; i <= 100; i++) {
+		  promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then(res => res.json()));
+		}
 
-
-function getPokemons() {
-	fetch('https://pokeapi.co/api/v2/pokemon/?results=10')
-		.then(res => res.json())
-		.then(data => {
-			let pokemeons = data.results ;
-				 
-			    return  pokemeons.map(function(pokemon){
-				console.log(pokemon)
-				var li = document.createElement('li');
-				var t = document.createTextNode(pokemon.name);
-				li.appendChild(t);
-				document.getElementById('pokedex').appendChild(li);
-			})
-			
-			
+		Promise.all(promises).then(results => {
+		  const pokemon = results.map(data => ({
+		    name: data.name,
+		    id: data.id,
+		    type: data.types.map(type => type.type.name).join(", "),
+		  }));
+		  displayPokemon(pokemon);
 		});
-}
+
+		const displayPokemon = pokemon => {
+			  console.log(pokemon);
+			  const pokemonData = pokemon
+			    .map(
+			      pokeman =>
+			        `
+			    <li>
+			        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+			        <p class="card-subtitle">Type: ${pokeman.type}</p>
+			    </li>
+			    `
+			    )
+			    .join("");
+			  pokedex.innerHTML = pokemonData;
+    };
+ pokedex.innerHTML = pokemonData;
